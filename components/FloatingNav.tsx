67602bfc9a9df5913b2564ceb7ae51d02ed37f42
Bautista Alós Martorell <bautialos9@gmail.com>
2025-01-28 19:03:20 -0300

    committt

--- components/FloatingNav.tsx
+++ components/FloatingNav.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { JSX, useState } from "react";
+import React, { useState } from "react";
 import {
   motion,
   AnimatePresence,
@@ -16,7 +16,7 @@ export const FloatingNav = ({
   navItems: {
     name: string;
     link: string;
-    icon?: JSX.Element;
+    icon?: React.ReactNode;
   }[];
   className?: string;
 }) => {
