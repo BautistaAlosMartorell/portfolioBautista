e44f762fcc89a105240f2bd2f1322c182c1d2d3a
Bautista Alós Martorell <bautialos9@gmail.com>
2025-01-28 18:52:34 -0300

    third commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "prefer-const": "off", // Desactivar preferencia por const
+      "@typescript-eslint/no-explicit-any": "off", // Desactivar el uso de any
+    },
+  },
 ];
 
 export default eslintConfig;
