a34e437308338d197da4e87b16126b33fb6b2518
Bautista Alós Martorell <bautialos9@gmail.com>
2025-01-28 18:42:40 -0300

    first commit

---
+++ data/index.ts
@@ -0,0 +1,135 @@
+export const navItems = [
+    { name: "Sobre mí", link: "#about" },
+    { name: "Proyectos", link: "#projects" },
+    { name: "Contacto", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "Me apasiona resolver problemas y trabajar en equipo.",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "Siempre estoy buscando nuevas oportunidades para mejorar.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "Tecnologías: ",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Me gusta aprender nuevas herramientas para mejorar lo que desarrollo.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "desarrollo mi portafolio y nuevos proyectos.",
+      description: "Actualmente,",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "¿Quieres iniciar un proyecto juntos?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const  projects = [
+    {
+      id: 1,
+      title: "ThreeJS Shop",
+      des: "Página interactiva para diseñar remeras en 3D con personalización de colores e imágenes, integrada con IA para diseños únicos.",
+      img: "/threejsimg.png",
+      iconLists: ["/re.svg", "/tail.svg", "/js.svg", "/three.svg"],
+      link: "https://github.com/BautistaAlosMartorell/ThreejsShop.git",
+    },
+    {
+      id: 2,
+      title: "Nike Landing Page",
+      des: "Clon responsive de la página principal de Nike, replicando su diseño y funcionalidad.",
+      img: "/nikeimg.png",
+      iconLists: ["/re.svg", "/tail.svg", "/js.svg"],
+      link: "https://github.com/BautistaAlosMartorell/NikeLandingPage.git",
+    },
+    {
+      id: 3,
+      title: "E-Commerce",
+      des: "Plataforma para gestionar productos con funcionalidades de alta, baja, modificación y búsqueda dinámica.",
+      img: "ecommerceimg.png",
+      iconLists: ["/js.svg", "/html.svg", "/css.svg"],
+      link: "https://github.com/BautistaAlosMartorell/IntegradorFrontEnd.git",
+    },
+    {
+      id: 4,
+      title: "Tetris",
+      des: "Clon del clásico Tetris, con controles intuitivos y mecánica fluida.",
+      img: "/tetrisimg.png",
+      iconLists: ["/csharp.svg", "/unity.svg"],
+      link: "https://github.com/BautistaAlosMartorell/TetrisClone.git",
+    },
+    {
+        id: 5,
+        title: "Shadow Dash",
+        des: "Videojuego infinito donde controlás a un personaje que debe esquivar obstáculos que caen del cielo. Completamente diseñado, animado y programado por mí desde cero.",
+        img: "/shadowdashimg.png",
+        iconLists: ["/csharp.svg", "/unity.svg"],
+        link: "https://github.com/BautistaAlosMartorell/InfiniteRunner2D.git",
+      },
+      {
+        id: 6,
+        title: "Chrome Dino",
+        des: "Recreación del famoso dinosaurio de Google, con mecánicas simples y diseño minimalista.",
+        img: "/dinogameimg.jpg",
+        iconLists: ["/csharp.svg", "/unity.svg"],
+        link: "https://github.com/BautistaAlosMartorell/DinoGameClone.git",
+      },
+  ];
+  
+
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/BautistaAlosMartorell"
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link: "https://www.linkedin.com/in/bautistaalosmartorell"
+    },
+  ];
